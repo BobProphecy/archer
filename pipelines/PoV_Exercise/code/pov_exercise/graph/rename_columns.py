364d7a107321adec6374160bac1f7fa3b6ab2e42
bobw@prophecy.io <bobw@prophecy.io>
2024-08-14 15:02:04 +0000

    Adds JobTest1 and refactors PoV_Exercise pipeline - Adds JobTest1 with Databricks configuration - Removes unused transformations in PoV_Exercise - Adds rename_columns transformation to PoV_Exercise

---
+++ pipelines/PoV_Exercise/code/pov_exercise/graph/rename_columns.py
@@ -0,0 +1,17 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.utils import *
+from prophecy.libs import typed_lit
+from pov_exercise.config.ConfigStore import *
+from pov_exercise.functions import *
+
+def rename_columns(spark: SparkSession, in0: DataFrame, in1: DataFrame) -> DataFrame:
+    # Extract the renaming mappings from rename_col
+    rename_mappings = {row['SourceColumn']: row['TargetColumn'] for row in rename_col\
+                            .collect(
+                          )}
+    # Rename columns in out0 based on the mappings
+    out0 = data.withColumnsRenamed(rename_mappings)
+
+    return out0
